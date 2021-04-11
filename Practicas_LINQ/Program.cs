8eb1459dacc14474ea76bc8ddbe3dde3f0b62429
Sebastián Serrisuela <sebaserri@gmail.com>
2021-04-10 21:49:54 -0300

    S4C13 Mostrar estudiantes cuya materia sea Programación o Matemáticas.

--- Practicas_LINQ/Program.cs
+++ Practicas_LINQ/Program.cs
@@ -48,11 +48,19 @@ namespace Practicas_LINQ
             //}
 
             //Mostrar estudiantes cuya nota 1 sea mayor que 6 y cuya nota 2 sea mayor a 7
-            var resultado = estudiantes.Where( x=> x.Nota1 > 6 && x.Nota2 > 7).ToList();
+            //var resultado = estudiantes.Where( x=> x.Nota1 > 6 && x.Nota2 > 7).ToList();
+
+            //foreach (var item in resultado)
+            //{
+            //    Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}, Nota 1: {item.Nota1}, Nota 2: {item.Nota2}");
+            //}
+
+            //Mostrar estudiantes cuya materia sea Programación o Matemáticas
+            var resultado = estudiantes.Where(x => x.Materia.Contains("Programación") || x.Materia.Contains("Matemáticas")).ToList();
 
             foreach (var item in resultado)
             {
-                Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}, Nota 1: {item.Nota1}, Nota 2: {item.Nota2}");
+                Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}");
             }
 
             Console.ReadKey();
