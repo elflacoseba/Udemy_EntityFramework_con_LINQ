e72b124ce7fa7e430d83e7139c83c498cede6d76
Sebastián Serrisuela <sebaserri@gmail.com>
2021-04-12 20:49:24 -0300

    Separo en archivos independientes cada lección. Separo la Sección 4, Clase 12: Select()

---
+++ Practicas_LINQ/Clases/S4C12.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practicas_LINQ.Models;
+using Practicas_LINQ.Repositorio;
+
+namespace Practicas_LINQ.Clases
+{
+    public class S4C12
+    {
+        public void MuestraDatos()
+        {
+            List<Estudiante> estudiantes = new EstudianteRepositorio().GetEstudiantes();
+
+            //select() nos permite seleccionar campos específicos
+
+            var e = estudiantes.Select(x => new { x.Id, x.Nombre, x.Materia });
+
+            foreach (var item in e)
+            {
+                Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}");
+            }
+        }
+
+
+    }
+}
--- Practicas_LINQ/Program.cs
+++ Practicas_LINQ/Program.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Practicas_LINQ.Models;
-using Practicas_LINQ.Repositorio;
+using Practicas_LINQ.Clases;
 
 namespace Practicas_LINQ
 {
@@ -12,56 +11,11 @@ namespace Practicas_LINQ
     {
         static void Main(string[] args)
         {
-            List<Estudiante> estudiantes = new EstudianteRepositorio().GetEstudiantes();
 
-            //foreach (Estudiante e in estudiantes )
-            //{
-            //    Console.WriteLine($"ID: {e.Id.ToString().PadLeft(2,' ')},\t Nombre: {(e.Nombre + ",").PadRight(20,' ')} Materia: {e.Materia}");
-            //}
+            //SELECT()
+            S4C12 leccion = new S4C12();
 
-            //select() nos permite seleccionar campos específicos
-
-            //var e = estudiantes.Select( x => new { x.Id, x.Nombre, x.Materia });
-
-            //foreach (var item in e)
-            //{
-            //    Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}");
-            //}
-
-            //where nos permite filtrar resultados de la consulta en base a evaluaciones que nosotros proporcionemos
-            //Seleccionar unicamente materia programación
-
-            //var resultado = estudiantes.Where(x => x.Materia.Contains("Programación")).ToList();
-
-            //foreach (var item in resultado)
-            //{
-            //    Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}");
-            //}
-
-            //Mostrar Estudiantes con Nota 1 mayor o igual a 7
-
-            //var resultado = estudiantes.Where(x => x.Nota1 >= 7).ToList();
-
-            //foreach (var item in resultado)
-            //{
-            //    Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}, Nota 1: {item.Nota1}");
-            //}
-
-            //Mostrar estudiantes cuya nota 1 sea mayor que 6 y cuya nota 2 sea mayor a 7
-            //var resultado = estudiantes.Where( x=> x.Nota1 > 6 && x.Nota2 > 7).ToList();
-
-            //foreach (var item in resultado)
-            //{
-            //    Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}, Nota 1: {item.Nota1}, Nota 2: {item.Nota2}");
-            //}
-
-            //Mostrar estudiantes cuya materia sea Programación o Matemáticas
-            var resultado = estudiantes.Where(x => x.Materia.Contains("Programación") || x.Materia.Contains("Matemáticas")).ToList();
-
-            foreach (var item in resultado)
-            {
-                Console.WriteLine($"ID: {item.Id}, Nombre: {item.Nombre}, Materia: {item.Materia}");
-            }
+            leccion.MuestraDatos();
 
             Console.ReadKey();
         }
